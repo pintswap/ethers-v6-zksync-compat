41acb753c4fcb2671a4c71961b8e7beafbb3c916
pintswap <dev@pintswap.exchange>
2023-05-01 14:19:47 -0500

    update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,1 @@
-export * from "./polyfill";
+export * from "./provider";
--- lib/polyfill.d.ts
+++ lib/polyfill.d.ts
@@ -1,4 +1,11 @@
 import "ethers";
-declare const formatTransactionReceipt: any;
-export { formatTransactionReceipt };
-export declare const formatTransactionReceiptShim: any;
+export type FormatFunc = (value: any) => any;
+export declare function allowNull(format: FormatFunc, nullValue?: any): FormatFunc;
+export declare function arrayOf(format: FormatFunc): FormatFunc;
+export declare function object(format: Record<string, FormatFunc>, altNames?: Record<string, Array<string>>): FormatFunc;
+export declare function formatBoolean(value: any): boolean;
+export declare function formatData(value: string): string;
+export declare function formatHash(value: any): string;
+export declare function formatUint256(value: any): string;
+export declare const formatReceiptLog: FormatFunc;
+export declare const wrapTransactionReceipt: FormatFunc;
